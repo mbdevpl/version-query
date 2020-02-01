5c171b96d56553c4f19ffe6bd339a86356a2a2e1
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2020-02-01 21:51:48 +0900

    removed deprecated function

--- version_query/__init__.py
+++ version_query/__init__.py
@@ -8,12 +8,3 @@ from .version import VersionComponent, Version
 from .query import query_folder, query_caller, query_version_str
 from .git_query import predict_git_repo
 from .query import predict_caller, predict_version_str
-
-
-def generate_version_str(increment_if_repo_chnaged: bool = True) -> str:
-    """Generate the version string for current/upcoming version of the package."""
-    import warnings
-    warnings.warn('function generate_version_str() is deprecated, use predict() instead',
-                  DeprecationWarning, stacklevel=2)
-    from .query import predict_caller
-    return predict_caller(2).to_str()
