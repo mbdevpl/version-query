bae8823187417529087a5d88148612d73166bedd
Mateusz Bysiek <1270332+mbdevpl@users.noreply.github.com>
2026-05-06 18:03:13 +0900

    chore: don't support 3.9

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ class Package(boilerplates.setup.Package):
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
